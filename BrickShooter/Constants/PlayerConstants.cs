3587f343d2ba22821279b8e3a2d9237b6e276364
Kiryl Prykhodzka <kirilkin.97@gmail.com>
2023-09-17 21:39:12 +0200

    adjust movement related constants

--- BrickShooter/Constants/PlayerConstants.cs
+++ BrickShooter/Constants/PlayerConstants.cs
@@ -2,12 +2,12 @@
 {
     public class PlayerConstants
     {
-        public const int MAX_VELOCITY = 250;
+        public const int MAX_VELOCITY = 300;
         //when player velocity goes below this value, it will reset to zero
         public const int MIN_VELOCITY = 10;
         //max velocity will be reached in 5 updates
-        public const float ACCELERATION_FACTOR = 0.2f;
+        public const float ACCELERATION_FACTOR = 0.1f;
         //player decelerates twice as fast as accelerates
-        public const float DECELERATION_FACTOR = ACCELERATION_FACTOR * 2;
+        public const float DECELERATION_FACTOR = 0.2f;
     }
 }
