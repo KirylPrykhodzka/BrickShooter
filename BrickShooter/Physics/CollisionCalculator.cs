4964b5367df6b98be90110a0fb6a8b662fbf3387
Kiryl Prykhodzka <kirilkin.97@gmail.com>
2023-10-01 11:47:12 +0200

    correctly find front facing points of a polygon

--- BrickShooter/Physics/CollisionCalculator.cs
+++ BrickShooter/Physics/CollisionCalculator.cs
@@ -28,15 +28,17 @@ namespace BrickShooter.Physics
 
         public static IList<Vector2> GetFrontFacingPoints(MaterialObject materialObject)
         {
+
+            if (materialObject.Velocity == Vector2.Zero || materialObject.LocalColliderPolygon.Points.Count <= 2)
+            {
+                return materialObject.LocalColliderPolygon.Points;
+            }
+
             var localColliderPoints = materialObject.LocalColliderPolygon.Points
                 //this makes sure that polygon center is in (0,0) which makes it much easier to operate on
                 .Select(x => x - materialObject.LocalColliderPolygon.Center)
                 .ToList();
 
-            if (materialObject.Velocity == Vector2.Zero || localColliderPoints.Count <= 2)
-            {
-                return localColliderPoints;
-            }
             var perpendicularAxis = new Vector2(materialObject.Velocity.Y, -materialObject.Velocity.X);
             char projectionComparisonAxis = Math.Abs(perpendicularAxis.X) > Math.Abs(perpendicularAxis.Y) ? 'x' : 'y';
 
@@ -45,8 +47,10 @@ namespace BrickShooter.Physics
                 //need to add * new Vector2(1, -1) because in Monogame Y axis is inverted
                 .Select(x => (x, x.Project(perpendicularAxis)));
 
+            //left- and right-most points of the polygon relative to its velocity
             Vector2 min = Vector2.Zero;
             Vector2 max = Vector2.Zero;
+
             if (projectionComparisonAxis == 'x')
             {
                 var minX = perpendicularProjections.Min(x => x.value.X);
@@ -82,20 +86,31 @@ namespace BrickShooter.Physics
                 }
             }
 
-            var localizedResult = new List<Vector2> { min, max };
+            //find all points closer to the front then min and max
+            var result = new List<Vector2> { min, max };
+            var otherLocalColliderPoints = localColliderPoints.Where(x => x != min && x != max);
 
-            if(projectionComparisonAxis == 'y')
+            if (projectionComparisonAxis == 'y')
             {
-                var aboveY = localColliderPoints.Where(x => x.Y < 0);
-                var belowY = localColliderPoints.Where(x => x.Y >= 0);
+                var aboveY = otherLocalColliderPoints.Where(x => x.Y < 0);
+                var belowY = otherLocalColliderPoints.Where(x => x.Y >= 0);
                 var distanceToMin = materialObject.Velocity.X - min.X;
                 var distanceToMax = materialObject.Velocity.X - max.X;
-                localizedResult.AddRange(aboveY.Where(x => Math.Abs(materialObject.Velocity.X - x.X) < Math.Abs(distanceToMin)));
-                localizedResult.AddRange(belowY.Where(x => Math.Abs(materialObject.Velocity.X - x.X) < distanceToMax));
+                result.AddRange(aboveY.Where(x => Math.Abs(materialObject.Velocity.X - x.X) < Math.Abs(distanceToMin)));
+                result.AddRange(belowY.Where(x => Math.Abs(materialObject.Velocity.X - x.X) < distanceToMax));
+            }
+            else
+            {
+                var leftOfX = otherLocalColliderPoints.Where(x => x.X < 0);
+                var rightToX = otherLocalColliderPoints.Where(x => x.X >= 0);
+                var distanceToMin = materialObject.Velocity.Y - min.Y;
+                var distanceToMax = materialObject.Velocity.Y - max.Y;
+                result.AddRange(leftOfX.Where(x => Math.Abs(materialObject.Velocity.Y - x.Y) < Math.Abs(distanceToMin)));
+                result.AddRange(rightToX.Where(x => Math.Abs(materialObject.Velocity.Y - x.Y) < distanceToMax));
             }
 
             //need to "move" polygon back to its original position relative to object's position before returning
-            var result = localizedResult.Select(x => x + materialObject.LocalColliderPolygon.Center).ToList();
+            var result = result.Select(x => x + materialObject.LocalColliderPolygon.Center).ToList();
             return result;
         }
     }
